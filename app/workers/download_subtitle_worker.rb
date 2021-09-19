954b86530d880a1a420092cba2ee4bb9e203142b
kawanji01 <kawanji01@gmail.com>
2021-09-19 17:44:31 +0900

    Enhance downloading captions process

--- app/workers/download_subtitle_worker.rb
+++ app/workers/download_subtitle_worker.rb
@@ -2,25 +2,17 @@ class DownloadSubtitleWorker
   include Sidekiq::Worker
   sidekiq_options queue: :often
 
-  def perform(url, lang_code, token, file_type, locale)
+  def perform(url, lang_code, token, locale)
     csv = Youtube.scrape_caption_csv(url, lang_code)
     return if csv.blank?
 
-    case file_type
-    when 'csv'
-      file = csv
-    when 'srt'
-      # CSVをSRTに変換する。
-      file = Youtube.convert_csv_into_srt(csv)
-    when 'txt'
-      # 文字起こしのみをTXTに変換する。
-      file = Youtube.convert_csv_into_txt(csv)
-    end
-
+    srt = Youtube.convert_csv_into_srt(csv)
+    txt = Youtube.convert_csv_into_txt(csv)
     ActionCable.server.broadcast 'download_file_channel',
                                  token: token,
-                                 file: file,
-                                 file_type: file_type,
+                                 csv: csv,
+                                 srt: srt,
+                                 txt: txt,
                                  message: I18n.t('subtitles.download_completed', locale: locale)
   end
 end
